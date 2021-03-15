f30f732a400db5964983dc50bceb976168186993
JesusGerardo <jesusgerardo.1315@hotmail.com>
2021-03-14 20:19:14 -0600

    Generics List

--- src/uaslp/ingenieria/labs/Main.java
+++ src/uaslp/ingenieria/labs/Main.java
@@ -1,32 +1,38 @@
 package uaslp.ingenieria.labs;
 
-import uaslp.ingenieria.labs.list.Iterator;
 import uaslp.ingenieria.labs.list.LinkedList;
-import uaslp.ingenieria.labs.list.ReverseIterator;
+import uaslp.ingenieria.labs.list.Position;
 
 public class Main {
-
     public static void main(String[] args) {
 
-        LinkedList lista1 = new LinkedList();
+        int cuenta =  LinkedList.getListsCount();
+
+        LinkedList<Integer> lista1 = new LinkedList<>();
+        LinkedList<String> lista2 = new LinkedList<>();
 
         lista1.add(1);
         lista1.add(4);
         lista1.add(3);
         lista1.add(7);
 
-        Iterator it = lista1.getIterator();
+        lista2.add(" hola");
+        lista2.add(" Object ");
+        lista2.add(" Oriented ");
+        lista2.add(" Programming ");
+
+        LinkedList<Integer>.Iterator it = lista1.getIterator();
 
         while(it.hasNext()){
-            Iterator backupIt = new Iterator(it);
-            int element = it.next(); //regresar el dato y avanzar el iterador
+            LinkedList<Integer>.Iterator backupIt = lista1.new Iterator(it);
+            int element = it.next();
 
             if(element == 3){
-                lista1.insert(10, LinkedList.BEFORE, backupIt);
+                lista1.insert(10, Position.BEFORE, backupIt);
             }
 
             if(element == 7){
-                lista1.insert(15, LinkedList.AFTER, backupIt);
+                lista1.insert(15, Position.AFTER, backupIt);
             }
         }
 
@@ -46,13 +52,20 @@ public class Main {
 
         System.out.println("---------------------");
 
-        ReverseIterator reverseIterator = lista1.getReverseIterator();
+        LinkedList<Integer>.ReverseIterator reverseIterator = lista1.getReverseIterator();
 
-        while(reverseIterator.hasPrevious()){
-            int element = reverseIterator.previous();
+        while(reverseIterator.hasNext()){
+            int element = reverseIterator.next();
 
             System.out.println("Dato: " + element);
         }
 
+        System.out.println("---------------------");
+
+        LinkedList<String>.Iterator itStrings = lista2.getIterator();
+
+        while(itStrings.hasNext()){
+            System.out.println(itStrings.next());
+        }
     }
 }
--- src/uaslp/ingenieria/labs/list/LinkedList.java
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -1,29 +1,87 @@
 package uaslp.ingenieria.labs.list;
 
-/**
- * Lista doblemente ligada
- */
-public class LinkedList {
+import static uaslp.ingenieria.labs.list.Position.AFTER;
+import static uaslp.ingenieria.labs.list.Position.BEFORE;
 
-    public static final int BEFORE = 0;
-    public static final int AFTER = 1;
+public class LinkedList<G> {
 
-    private Node head;
-    private Node tail;
+    private static class Node<T>{
+        private final T data;
+        private Node<T> previous;
+        private Node<T> next;
+
+        Node(T data){
+            this.data = data;
+        }
+    }
+
+    private Node<G> head;
+    private Node<G> tail;
     private int size;
 
-    /**
-     * Inserts data at the end of the list
-     *
-     * @param data Data to be inserted
-     */
-    public void add(int data) {
-        Node node = new Node(data);
+    public LinkedList() {
+        listsCount ++;
+    }
 
-        node.setPrevious(tail);
+    private static int listsCount = 0;
+
+    public static int getListsCount(){
+        return listsCount;
+    }
+
+    public class Iterator {
+        private Node<G> currentNode;
+
+        public Iterator() {
+            this.currentNode = head;
+        }
+
+        public Iterator(Iterator iterator){
+            currentNode = iterator.currentNode;
+        }
+
+        public boolean hasNext(){
+            return currentNode != null;
+        }
+
+        public G next(){
+            G data = currentNode.data;
+            currentNode = currentNode.next;
+            return data;
+        }
+
+        Node<G> getCurrentNode() {  // modificador de acceso se llama -> package-private
+            return currentNode;
+        }
+    }
+
+    public class ReverseIterator {
+
+        private Node<G> currentNode;
+
+        public ReverseIterator() {
+            this.currentNode = tail;
+        }
+
+
+        public boolean hasNext(){
+            return currentNode != null;
+        }
+
+        public G next(){
+            G data = currentNode.data;
+            currentNode = currentNode.previous;
+            return data;
+        }
+    }
+
+    public void add(G data) {
+        Node<G> node = new Node<>(data);
+
+        node.previous = tail;
 
         if (tail != null) {
-            tail.setNext(node);
+            tail.next = node;
         }
 
         if (head == null) {
@@ -34,24 +92,20 @@ public class LinkedList {
         size++;
     }
 
-    /**
-     * @param index 0-index
-     * @return data in index
-     */
-    public int get(int index) {
-        Node currentNode = head;
+    public G get(int index) {
+        Node<G> currentNode = head;
         int currentIndex = 0;
 
         while (currentIndex < index) {
-            currentNode = currentNode.getNext();
+            currentNode = currentNode.next;
             currentIndex++;
         }
 
-        return currentNode.getData();
+        return currentNode.data;
     }
 
     public void delete(int index) {
-        Node currentNode = head;
+        Node<G> currentNode = head;
         int currentIndex = 0;
 
         if (index < 0 || index >= size) {
@@ -67,52 +121,50 @@ public class LinkedList {
         }
 
         if (index == 0) {
-            head = head.getNext();
-            head.setPrevious(null);
+            head = head.next;
+            head.previous = null;
         }
 
         if (index == size) {
-            tail = tail.getPrevious();
-            tail.setNext(null);
+            tail = tail.previous;
+            tail.next = null;
         }
 
         if (index > 0 && index < size) {
             while (currentIndex < index) {
-                currentNode = currentNode.getNext();
+                currentNode = currentNode.next;
                 currentIndex++;
             }
-            currentNode.getPrevious().setNext(currentNode.getNext());
-            currentNode.getNext().setPrevious(currentNode.getPrevious());
+            currentNode.previous.next = currentNode.next;
+            currentNode.next.previous = currentNode.previous;
         }
 
 
     }
 
     public Iterator getIterator() {
-        return new Iterator(head);
+        return new Iterator();
     }
 
-    public void insert(int data, int position, Iterator it) {
-        // ¿qué ofrece java para restringir los valores de position a solamente BEFORE y AFTER?
-
-        Node newNode = new Node(data);
-        Node currentNode = it.getCurrentNode();
+    public void insert(G data, Position position, Iterator it) {
+        Node<G> newNode = new Node<>(data);
+        Node<G> currentNode = it.getCurrentNode();
 
         if (position == AFTER) {
-            newNode.setNext(currentNode.getNext());
-            newNode.setPrevious(currentNode);
-            currentNode.setNext(newNode);
-            if (newNode.getNext() != null) {
-                newNode.getNext().setPrevious(newNode);
+            newNode.next = currentNode.next;
+            newNode.previous = currentNode;
+            currentNode.next = newNode;
+            if (newNode.next != null) {
+                newNode.next.previous = newNode;
             } else {
                 tail = newNode;
             }
         } else if (position == BEFORE) {
-            newNode.setPrevious(currentNode.getPrevious());
-            newNode.setNext(currentNode);
-            currentNode.setPrevious(newNode);
-            if (newNode.getPrevious() != null) {
-                newNode.getPrevious().setNext(newNode);
+            newNode.previous = currentNode.previous;
+            newNode.next = currentNode;
+            currentNode.previous = newNode;
+            if (newNode.previous != null) {
+                newNode.previous.next = newNode;
             } else {
                 head = newNode;
             }
@@ -122,14 +174,11 @@ public class LinkedList {
         size++;
     }
 
-
-    //Iterador -> patrón de diseño
-
     public int getSize() {
         return size;
     }
 
     public ReverseIterator getReverseIterator() {
-        return new ReverseIterator(tail);
+        return new ReverseIterator();
     }
 }
